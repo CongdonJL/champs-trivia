f11d7124e8147516a366da5560423d24d968f69c
Jeremy Congdon <jcongderulo@tgcs-mbp.lan>
2022-04-18 23:11:44 -0400

    Add Post Endpoint

--- server.js
+++ server.js
@@ -10,24 +10,11 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
-// var MONGODB_URI = "mongodb+srv://JCongdon:IVUThQCCmnoXUeHe@cluster0.faa1c.mongodb.net/questions?retryWrites=true&w=majority";
-
-// Connect to the database before starting the application server.
-// mongodb.MongoClient.connect(MONGODB_URI, function (err, database) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
-
-
 var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://JCongdon:IVUThQCCmnoXUeHe@cluster0-shard-00-00.faa1c.mongodb.net:27017,cluster0-shard-00-01.faa1c.mongodb.net:27017,cluster0-shard-00-02.faa1c.mongodb.net:27017/questions?ssl=true&replicaSet=atlas-b9yuoo-shard-0&authSource=admin&retryWrites=true&w=majority";
 MongoClient.connect(uri, function(err, client) {
   const collection = client.db("questions").collection("questionsCollection");
-  // perform actions on the collection object
-  // client.close();
 
   // Save database object from the callback for reuse.
   db = client;
@@ -54,7 +41,8 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/questions", function(req, res) {
-  db.collection(QUESTIONS_COLLECTION).find({question: 'test'}).toArray(function(err, docs) {
+
+  db.collection(QUESTIONS_COLLECTION).find().toArray(function(err, docs) {
     if (err) {
       console.log(err.message);
       handleError(res, err.message, "Failed to get contacts.");
@@ -65,18 +53,18 @@ app.get("/questions", function(req, res) {
 });
 
 app.post("/questions", function(req, res) {
-  var newQuestion = req.body;
-  newQuestion.createDate = new Date();
+  var question = req.body;
 
-  if (!(req.body.question && req.body.answer)) {
+  if (!(req.body.question)) {
     handleError(res, "Invalid user input", "Must provide a question and answer.", 400);
   }
 
-  db.collection(QUESTIONS_COLLECTION).insertOne(newQuestion, function(err, doc) {
+ db.collection(QUESTIONS_COLLECTION).find({Question: req.body.question}).toArray(function(err, result) {
     if (err) {
-      handleError(res, err.message, "Failed to create new Question.");
+      console.log(err.message);
+      handleError(res, err.message, "No Answer");
     } else {
-      res.status(201).json(doc.ops[0]);
+      res.status(200).json(result);
     }
   });
 });
@@ -86,12 +74,6 @@ app.post("/questions", function(req, res) {
  *    PUT: update question by question
  *    DELETE: deletes question by id
  */
-
-app.get("/questions/:question", function(req, res) {
-});
-
-app.put("/questions/:question", function(req, res) {
-});
 
 app.delete("/questions/:id", function(req, res) {
 });
