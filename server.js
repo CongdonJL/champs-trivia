192ce0efe82ce753e20a9ee67c2c5ad21ab5c917
Jeremy Congdon <jcongderulo@tgcs-mbp.lan>
2022-04-18 21:08:02 -0400

    Fix DB String

--- server.js
+++ server.js
@@ -10,26 +10,35 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-var MONGODB_URI = "mongodb://JCongdon:IVUThQCCmnoXUeHe@cluster0.faa1c.mongodb.net/qustions?retryWrites=true&w=majority;"
+// // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+// var db;
+// var MONGODB_URI = "mongodb://JCongdon:IVUThQCCmnoXUeHe@cluster0.faa1c.mongodb.net/qustions?retryWrites=true&w=majority;"
 
-// Connect to the database before starting the application server.
-Mongodb.MongoClient.connect(MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+// // Connect to the database before starting the application server.
+// Mongodb.MongoClient.connect(MONGODB_URI, function (err, database) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
+//   // Save database object from the callback for reuse.
+//   db = database;
+//   console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port); 
-  });
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port); 
+//   });
+// });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://JCongdon:IVUThQCCmnoXUeHe@cluster0.faa1c.mongodb.net/questions?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("questions").collection("questionsColletion");
+  // perform actions on the collection object
+  client.close();
 });
 
 // questions API ROUTES BELOW
