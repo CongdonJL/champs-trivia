6171f47fd6b0f3c922299ff902795b3acf01dee0
Jeremy Congdon <jcongderulo@tgcs-mbp.lan>
2022-04-18 23:17:53 -0400

    get request body

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ MongoClient.connect(uri, function(err, client) {
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
+  console.log("here");
   res.status(code || 500).json({"error": message});
 }
 
