456b62eff85a2fdd283cc109afb301e7238513e9
Jeremy Congdon <jcongderulo@tgcs-mbp.lan>
2022-04-22 22:42:14 -0400

    Fix Spelling Errors

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.delete("/questions/:id", function(req, res) {
 
 
 app.delete("/duplicates", function(req, res) {
-  db.collectioncollection('QUESTIONS_COLLECTION').find({}, {Question:1}).sort({_id:1}).forEach(function(doc){
+  db.collection('QUESTIONS_COLLECTION').find({}, {Question:1}).sort({_id:1}).forEach(function(doc){
     db.collection('QUESTIONS_COLLECTION').remove({_id:{$gt:doc._id}, Question:doc.Question});
   })
 });
