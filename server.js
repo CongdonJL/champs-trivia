1b9ed521b1408eb85035294a2b333eac18541d1f
Jeremy Congdon <jcongderulo@tgcs-mbp.lan>
2022-11-22 07:25:24 -0500

    Add energy Cost endpoint

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
 
 var QUESTIONS_COLLECTION = "questionsCollection";
 var MISSING_QUESTIONS_COLLECTION = "missingQuestionsCollection";
 
 
 var app = express();
+
+app.use(cors(corsOptions));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
