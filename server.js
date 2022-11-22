3849450a88363045119d8323a6d536660cf598ac
Jeremy Congdon <jcongderulo@tgcs-mbp.lan>
2022-11-21 23:32:03 -0500

    Add energy Cost endpoint

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ app.get("/update", function(req, res) {
 app.get("/energyCost", function(req, res) {
 
 
-return {"status":"success","statusCode":200,"data":{"status":"available"}};
+return json({"status":"success","statusCode":200,"data":{"energyCost":1,"reward":100}});
 
 })
 
