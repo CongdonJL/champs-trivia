f6954939ea19802070de67fc244d4d68ab6133b2
Jeremy Congdon <jcongderulo@tgcs-mbp.lan>
2023-10-20 17:30:23 -0400

    Update: Add Logging

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ app.get("/v2/questions", function(req, res) {
     handleError(res, "Invalid user input", "Must provide a question.", 400);
   } else {
    db.find({Question: question}).toArray(function(err, result) {
+    console.log(result);
     res.status(200).json(result);
    });
  }
